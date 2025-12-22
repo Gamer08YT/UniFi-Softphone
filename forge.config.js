063adc63bb95d9740d24ffd8079773c5773e6a40
Jan Heil <jan.heil@byte-store.de>
2025-12-22 17:40:06 +0100

    Update MSIX packaging configuration: adjust publisher info, app identity, and minimum OS version

--- forge.config.js
+++ forge.config.js
@@ -6,13 +6,17 @@ module.exports = {
         {
             name: '@electron-forge/maker-msix',
             sign: true,
+            windowsKitPath: "C:\\Program Files (x86)\\Windows Kits\\10\\bin\\10.0.26100.0\x64",
             config: {
                 packageAssets: 'D:\\Programmieren\\UniFiSoftphone\\build\\appx',
                 manifestVariables: {
-                    publisherDisplayName: 'Jan Heil (www.byte-store.de)',
+                    publisherDisplayName: 'BYTESTORE',
                     appDisplayName: "Unofficial UniFi Talk Softphone",
                     packageBackgroundColor: '#131517',
-                    publisher: 'CN=555309DB-480B-42F6-B556-988555579009'
+                    publisher: 'CN=555309DB-480B-42F6-B556-988555579009',
+                    packageMinOSVersion: "10.0.26100.0",
+                    packageName: "37017jaxnprivate.UnofficialUniFiTalkSoftphone",
+                    packageIdentity: "37017jaxnprivate.UnofficialUniFiTalkSoftphone"
                 }
             }
         }
